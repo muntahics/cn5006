5925fc54091a0c56b3604e585c536c1e338836a1
muntahics <muntahi05@outlook.com>
2025-10-02 16:29:36 +0000

    initial push

---
+++ week1/portfolio/index.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")()
+
+const num1 = parseInt(prompt("Enter your first number: "))
+const num2 = parseInt(prompt("Enter your second number: "))
+const method = prompt("Enter your method: ")
+
+switch (method) {
+    case '+': console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}`);break;
+    case '-': console.log(`The difference of ${num1} and ${num2} is ${num1 - num2}`);break;
+    case '/': console.log(`The division of ${num1} and ${num2} is ${num1 / num2}`);break;
+    case '*': console.log(`The multiplication of ${num1} and ${num2} is ${num1 * num2}`);break;
+    default: console.log(`Please enter a method from these four +,-,/,*`); 
+}
+
