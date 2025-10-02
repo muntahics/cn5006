a4682c7890a31a36aedd2940fbb67018a9d471ea
muntahics <muntahi05@outlook.com>
2025-10-02 16:01:55 +0000

    First Exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const prompt = require("prompt-sync")()
+console.log("Starting")
+const name = prompt("Enter your name: ")
+console.log(`Hello, ${name}`)
+
+const number = parseInt(prompt("Enter a number: "))
+
+if(number > 0){
+    console.log("The number is positive")
+}
+else if(number === 0){
+    console.log("The number is zero")
+}
+else{
+    console.log("The number is negative")
+}
